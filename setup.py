97425fcacddb34d7f86777296c293493100bbdb1
akhiilkasare <akhil.kasare80@gmail.com>
2021-11-29 00:10:53 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="its a mlops package", 
+    author="Akhil",
+    packages=find_packages(),
+    license="MIT",
+)
